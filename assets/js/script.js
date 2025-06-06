6f77360b83e665247991579112a04343a47552aa
FrancescoSls97 <frankds1997@gmail.com>
2025-06-06 18:09:50 +0200

    js data log

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
+// definizione variabili
+const formEl = document.querySelector("form");
+const user = document.getElementById("userFullName");
+const kmNumber = document.getElementById("travelKM");
+const userAge = document.getElementById("ageSelect");
 
+//verifica log variabili
+
+console.log(formEl, user, kmNumber, userAge);
+
+// creare interazione con il bottone
+
+formEl.addEventListener("submit", (e) => {
+  //prevenire il comportamento default del browser
+  e.preventDefault();
+
+  const userInput = user.value;
+  const kmInput = kmNumber.value;
+  const ageInput = userAge.value;
+
+  console.log(userInput, kmInput, ageInput);
+});
