4fb37cc9b90cdb96b7def85c9873bdf83dff6e67
FrancescoSls97 <frankds1997@gmail.com>
2025-06-07 17:49:24 +0200

    add offer define

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,12 @@ const formEl = document.querySelector("form");
 const user = document.getElementById("userFullName");
 const kmNumber = document.getElementById("travelKM");
 const userAge = document.getElementById("ageSelect");
+const wagon = Math.floor(Math.random() * (12 + 1));
+const cp_code = Math.floor(Math.random() * (99999 + 1));
 
 //verifica log variabili
 
-console.log(formEl, user, kmNumber, userAge);
+console.log(formEl, user, kmNumber, userAge, wagon, cp);
 
 // creare interazione con il bottone
 
@@ -14,9 +16,35 @@ formEl.addEventListener("submit", (e) => {
   //prevenire il comportamento default del browser
   e.preventDefault();
 
+  //definizione valori
+
   const userInput = user.value;
   const kmInput = kmNumber.value;
   const ageInput = userAge.value;
 
   console.log(userInput, kmInput, ageInput);
+
+  //definizione offerta
+  if (userAge.value === "0-17 anni (Bambino)") {
+    console.log("Sconto under18");
+    offerType.innerText = "Sconto under18";
+  } else if (userAge.value === "65+ anni (Anziano)") {
+    console.log("conto over65");
+    offerType.innerText = "Sconto over65";
+  } else if (userAge.value === "18-64 anni (Adulto)") {
+    console.log("Standard:");
+    offerType.innerText = "Standerd";
+  }
+
+  //calcolo costo del biglietto
+
+  const price = kmNumber * 0.21;
+  const discount18 = 20;
+  const discount65 = 40;
+
+  // dati biglietto //
+
+  userName.innerText = user.value;
+  wagon_number.innerText = wagon;
+  cp.innerText = cp_code;
 });
